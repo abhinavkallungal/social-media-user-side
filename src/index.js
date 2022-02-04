446b1773a9454d1326960edb33dc38cbb1593850
Abhinav Kallungal <abhinavkallungal15@gmail.com>
2022-02-04 10:06:38 +0530

    google link removed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
